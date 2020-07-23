b0f5df9086a551d6f3bb6d585682f459b43a8d39
ivanpervushin <spontioffen23743>
2020-07-23 18:01:56 +0200

    :tada: init commit

---
+++ src/Providers/MouseflowCookieServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+namespace MouseflowCookie\Providers;
+
+use Plenty\Plugin\ServiceProvider;
+
+/**
+ * Class HelloWorldServiceProvider
+ * @package HelloWorld\Providers
+ */
+class MouseflowCookieServiceProvider extends ServiceProvider
+{
+
+	/**
+	 * Register the service provider.
+	 */
+	public function register()
+	{
+		$this->getApplication()->register(MouseflowCookieRouteServiceProvider::class);
+
+        /** @var ConsentRepositoryContract $consentRepository */
+        $consentRepository = pluginApp(ConsentRepositoryContract::class);
+
+        $consentRepository->registerConsent(
+            'mouseflowCookie',
+            'Mouseflow',
+            [
+                'description' => 'Mouseflow Cookies',
+                'provider' => 'Mouseflow ApS',
+                'lifespan' => '90 Days',
+                'policyUrl' => 'https://mouseflow.com/de/privacy/',
+                'group' => 'tracking',
+                'necessary' => 'false',
+                'isOptOut' => 'true',
+                'cookieNames' => ['mf_[session]', 'mf_user']
+            ]
+        );
+	}
+}
